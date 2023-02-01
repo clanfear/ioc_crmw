a84c8b28ea08b842d35f05c7409464405a3156e2
clanfear <cclanfear@gmail.com>
2023-02-01 13:54:24 +0000

    Slides and code

--- _lectures/2_quantitative-data-management/syntax/data_processing_example.R
+++ _lectures/2_quantitative-data-management/syntax/data_processing_example.R
@@ -1,52 +1,48 @@
 library(tidyverse)
 library(janitor)
+library(readxl)
 
-metro_2022_raw <- read_csv(list.files("./_lectures/2_quantitative-data-management/data/", recursive = TRUE, full.names = TRUE))
+# First challenge: Multiple files
+
+metro_2022_raw <- read_csv(list.files("./_lectures/2_quantitative-data-management/data/", pattern = "metropolitan", recursive = TRUE, full.names = TRUE))
 
 glimpse(metro_2022_raw)
 
 metro_2021_crime <- metro_2022_raw |>
-  clean_names()
+  clean_names() |>
+  filter(crime_type %in% c("Violence and sexual offences", "Robbery", "Burglary")) |>
+  count(lsoa_code, crime_type) |>
+  pivot_wider(names_from = crime_type, values_from = n) |>
+  clean_names() |>
+  mutate(across(-lsoa_code, ~ifelse(is.na(.), 0, .))) |>
+  rename(lsoa = lsoa_code)
 
 glimpse(metro_2021_crime)
 
-deptivation_2019_raw <- read_csv("./_lectures/2_quantitative-data-management/data/imd2019lsoa.csv")
+#London has 4,835 LSOAs so we have some extras!
 
+density_2019_raw <- read_excel("./_lectures/2_quantitative-data-management/data/land-area-population-density-lsoa11-msoa11.xlsx", sheet = 2)
 
-  mutate(borough = str_remove(lsoa_name, " [0-9].*$")) %>% 
-  group_by(borough) %>%
-  mutate(n = n()) %>%
-  ungroup() %>%
-  filter(n > 1000) %>%
-  filter(crime_type %in% c("Burglary", "Robbery", "Violence and sexual offences", "Anti-social behaviour")) %>%
-  mutate(crime_type = str_to_lower(str_replace_all(str_remove(crime_type, "-"), " ", "_"))) %>%
-  count(borough, crime_type, month) %>%
-  mutate(month = lubridate::ym(month)) %>%
-  pivot_wider(names_from = crime_type, values_from = n) 
+glimpse(density_2019_raw)
+density_2019 <- density_2019_raw |>
+  clean_names() |> 
+  select(lsoa = lsoa11_code, density = people_per_sq_km)
 
+deprivation_2019_raw <- read_csv("./_lectures/2_quantitative-data-management/data/imd2019lsoa.csv")
+glimpse(deprivation_2019_raw)
 
-borough_deprivation <- vroom::vroom("https://clanfeat.github.io/ioc_iqa/_data/source_data/indices-of-multiple-deprivation-borough.csv") %>%
-  select(borough = Area, 
-         avg_deprivation = `Average Score - 2007`) %>%
-  filter(borough %in% london_subregion$borough) %>%
-  mutate(dep = ntile(avg_deprivation, 3)) %>%
-  mutate(deprivation = case_when(
-    dep ==1 ~ "Low",
-    dep == 2 ~ "Medium",
-    dep == 3 ~ "High",
-    TRUE ~ "ERROR"
-  )) %>%
-  select(borough, deprivation, dep_score = avg_deprivation)
+deprivation_2019 <- deprivation_2019_raw |>
+  clean_names() |>
+  mutate(index = str_sub(indices_of_deprivation, 4, -1),
+         index = str_remove_all(index, "( )?(of |Deprivation|Domain|Index|Affecting|Barriers to |( \\(|and|,).*$)")) |>
+  filter(measurement == "Decile") |>
+  select(lsoa = feature_code, index, value) |>
+  pivot_wider(names_from = index, values_from = value) |>
+  clean_names()
 
-write_csv(borough_deprivation, file = "./_data/borough_deprivation.csv")
+# Why are some missing from crime data?
 
-borough_pop_density <- vroom::vroom("./_data/source_data/housing-density-borough.csv") %>%
-  filter(Year == 2021) %>%
-  select(borough = Name, pop = Population, area = Square_Kilometres) %>%
-  filter(borough %in% london_subregion$borough) |>
-  janitor::clean_names(case = "title")
-
-write_csv(borough_pop_density, file = "./_data/borough_pop_density.csv")
-
-borough_pop_density <- borough_pop_density |> janitor::clean_names()
+density_2019 |>
+  left_join(metro_2021_crime) |>
+  inner_join(deprivation_2019)
 
