9dfb59440f7d9d52954ec31a8a8307212598c552
Charles Lanfear <cclanfear@gmail.com>
2023-02-28 15:00:21 +0000

    Some progress

--- _lectures/assets/cam_macros.js
+++ _lectures/assets/cam_macros.js
@@ -2,3 +2,7 @@ remark.macros.width = function(w) {
   var url = this;
   return '<img src="' + url + '" style="width: ' + w + '" />';
 };
+remark.macros.height = function(h) {
+  var url = this;
+  return '<img src="' + url + '" style="height: ' + h + '" />';
+};
